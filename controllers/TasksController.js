fcbca016795bc7c9cd59831c1b772ee1e449bfd3
hala <halagrant@hotmail.com>
2020-02-09 22:33:36 +0200

    Updates in backend call

--- controllers/TasksController.js
+++ controllers/TasksController.js
@@ -1,10 +1,10 @@
 var models = require('../models/');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
-const fs = require('fs');
 
 // function getData to get all data
 exports.getData = function(req, res){
+
 
   // console.log(req.query);
 let query= {
@@ -12,6 +12,129 @@ let query= {
     limit: 10
   };
   models.in3.findAll({})
+  .then(function(client) {
+      res.json(client);
+  })
+  .catch(function(err) {
+      res.status(500).json(err.message);
+  });
+}
+
+
+// custimized search
+exports.getFeaturedData = function(req, res){
+let whereArr=[];
+let emptyCriteria=true;
+///check on company data
+if(req.body.Company&&req.body.Company!="")
+{
+  emptyCriteria=false;
+  whereArr.push({
+    Company:{
+            [Op.like]: "%"+ req.body.Company+"%"
+          }
+  });
+}
+// check on products
+if(req.body.Product&&req.body.Product!="")
+{
+  emptyCriteria=false;
+  whereArr.push({
+    Product:{
+            [Op.like]: "%"+ req.body.Product+"%"
+          }
+  });
+}
+
+// getting data of CPU
+if(req.body.CPU&&req.body.CPU!="")
+{
+    emptyCriteria=false;
+  let CPUWhere=[];
+  console.log("CPU");
+  req.body.CPU.split(",").forEach((item, i) => {
+    if(item!="")
+    {
+      CPUWhere.push({
+                [Op.like]:"%"+item+"%"
+              });
+    }
+
+  });
+  if(CPUWhere.length>0)
+{
+  whereArr.push({
+    CPU:{
+            [Op.or]: CPUWhere
+
+          }
+  });
+}
+}
+
+
+// getting data of RAM
+if(req.body.RAM&&req.body.RAM!="")
+{
+    emptyCriteria=false;
+  let RAMWhere=[];
+  req.body.RAM.split(",").forEach((item, i) => {
+    if(item!="")
+    {
+      RAMWhere.push({
+                [Op.like]:"%"+item+"%"
+              });
+    }
+
+  });
+  if(RAMWhere.length>0)
+{
+  whereArr.push({
+    RAM:{
+            [Op.or]: RAMWhere
+
+          }
+  });
+
+}
+}
+
+// getting data of OpSys
+if(req.body.OpSys&&req.body.OpSys!="")
+{
+    emptyCriteria=false;
+  let OpSysWhere=[];
+  req.body.OpSys.split(",").forEach((item, i) => {
+    if(item!="")
+    {
+      OpSysWhere.push({
+                [Op.like]:"%"+item+"%"
+              });
+    }
+
+  });
+  if(OpSysWhere.length>0)
+{
+  whereArr.push({
+    OpSys:{
+            [Op.or]: OpSysWhere
+
+          }
+  });
+
+}
+}
+let query= {
+  where:{
+     [Op.and]:whereArr,
+
+  }
+
+  };
+  console.log("empty: ",emptyCriteria);
+  if(emptyCriteria) // get all data in case of empty criteria
+      query={};
+  models.in3.findAll(query)
   .then(function(client) {
       res.json(client);
   })
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const router = express.Router();
 
-// load the Tasks control 
+// load the Tasks control
 const Tasks = require('../controllers/TasksController');
 
 //http://localhost:3333/API/V1/Tasks/getData/?Company=HP
 router.get('/getData',Tasks.getData);
+router.post('/getFeaturedData',Tasks.getFeaturedData);
 module.exports = router;
