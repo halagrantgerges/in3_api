7640768bf3e45f4b9ff29ed74e8bbb77fbf51124
hala <halagrant@hotmail.com>
2020-02-10 21:11:19 +0200

    Adding unit test

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const router = express.Router();
 // load the Tasks control
 const Tasks = require('../controllers/TasksController');
 
-//http://localhost:3333/API/V1/Tasks/getData/?Company=HP
+//http://localhost:3333/API/V1/Tasks/getData
 router.get('/getData',Tasks.getData);
+// custimized search call
 router.post('/getFeaturedData',Tasks.getFeaturedData);
 module.exports = router;
